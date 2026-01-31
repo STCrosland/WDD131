d08e9f349ce4bbaf6627ef75faaa3d53b2cbc586
STCrosland <spencertcrosland@gmail.com>
2026-01-31 02:40:37 -0700

    mobile site
    this version is now complete

---
+++ site.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  btn.classList.toggle('change');
+});
